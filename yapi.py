4da4e1dc4d102968baefbcc81a162b4b4d98983c
xxu <timxu2007@gmail.com>
2020-02-06 19:49:55 -0800

    init

--- yapi.py
+++ yapi.py
@@ -2,8 +2,19 @@ import datetime as dt
 import pandas as pd
 import pandas_datareader.data as web
 
+
+PRICER_INV = "INV"
+PRICER_ARB = "ARB"
+PRICER_HFT = "HFT"
+DB_ROOT_DIRECTORY = "C:/db/"
+DB_INV_HISTDATA_STK_BARS = DB_ROOT_DIRECTORY + "db_mgr/db_securities/inv/db_histdata/stock_bars/"
+
 start = dt.datetime(2000, 1, 1)
 end = dt.datetime.now()
 df = web.DataReader("SPY", 'yahoo', start, end)
+
+
+
+
 df.to_csv('SPY.csv')
 print(df.head(6))
