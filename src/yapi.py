56826917cb4c6054dfce2f2897f5299f4c726502
xxu <timxu2007@gmail.com>
2020-02-07 16:45:53 -0800

    init

--- src/yapi.py
+++ src/yapi.py
@@ -4,15 +4,32 @@ import pandas_datareader.data as web
 import xlrd
 from src import db_constants
 
-excel_watchlist_df = pd.read_excel(db_constants.TABLE_INV_WATCHLIST, sheet_name='watch_list')
-print(excel_watchlist_df[db_constants.WATCHLIST_HEADER[0]].tolist())
 
+def requestAllTickersBarDataAndSaveToDB(pricerType, startDate, endDate, barSize):
+    allTickers = getAllTickers(pricerType)
+    for ticker in allTickers:
+        requestSingleTickerBarDataAndSaveToDB(ticker, startDate, endDate, barSize)
+
+
+def getAllTickers(pricerType):
+    if db_constants.PRICER_INV == pricerType:
+        return pd.read_excel(db_constants.TABLE_INV_WATCHLIST, sheet_name='watch_list')[db_constants.WATCHLIST_HEADER[0]].tolist()
+    elif db_constants.PRICER_ARB == pricerType:
+        return pd.read_excel(db_constants.TABLE_ARB_WATCHLIST, sheet_name='watch_list')[db_constants.WATCHLIST_HEADER[0]].tolist()
+    elif db_constants.PRICER_HFT == pricerType:
+        return pd.read_excel(db_constants.TABLE_HFT_WATCHLIST, sheet_name='watch_list')[db_constants.WATCHLIST_HEADER[0]].tolist()
+
+def requestSingleTickerBarDataAndSaveToDB(ticker, startDate, endDate, barSize):
+    df = web.DataReader(ticker, 'yahoo', startDate, endDate)
+
+    df.to_excel(db_constants.DB_INV_HISTDATA_STK_BARS + barSize + "_bar_prices/" + ticker + '.xls')
+    print(df.head(6))
 
 
 start = dt.datetime(2000, 1, 1)
 end = dt.datetime.now()
-# symbol = "QQQ"
-# df = web.DataReader(symbol, 'yahoo', start, end)
-#
-# df.to_csv(symbol + '.csv')
-# print(df.head(6))
+barSize = 'd'
+
+requestAllTickersBarDataAndSaveToDB(db_constants.PRICER_INV, start, end, barSize)
+requestAllTickersBarDataAndSaveToDB(db_constants.PRICER_ARB, start, end, barSize)
+requestAllTickersBarDataAndSaveToDB(db_constants.PRICER_HFT, start, end, barSize)
